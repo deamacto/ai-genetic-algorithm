7729373a5add2f8f4bf4345a08a219e5ad7a1168
Asia <pszon.joanna@gmail.com>
2022-03-29 17:48:13 +0200

    added tournament ga

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 import dataConfig.DataType;
 import dataConfig.Difficulty;
 import input.DataReader;
-import model.FLOModel;
-import model.FactoryModel;
-import model.Population;
+import model.*;
 
 import java.util.ArrayList;
 
@@ -11,7 +9,34 @@ public class Main {
     public static void main(String[] args) {
         DataType dt = new DataType(Difficulty.EASY);
 
-        Population population = new Population(dt, 5, "test");
-        population.RouletteSelection();
+    }
+
+    public static Statistics TournamentPopulation(DataType dataType) {
+        String populationName  = "tournament";
+        Population population = new Population(dataType, 100, populationName);
+        ArrayList<FactoryEfficiency> bestFactories = new ArrayList<>();
+        ArrayList<FactoryEfficiency> worstFactories = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++) {
+            bestFactories.add(population.getFactories().findBestFactory());
+            worstFactories.add(population.getFactories().findWorstFactory());
+
+            ArrayList<FactoryEfficiency> newPopulation = new ArrayList<>();
+
+            for(int j = 0; j < population.getFactories().getFactories().size(); j++) {
+                newPopulation.add(population.tournamentSelection(20));
+            }
+
+            population = new Population(newPopulation, populationName);
+
+            population.crossover(0.3, populationName);
+            population.mutation(0.2, 1, populationName);
+        }
+
+        Factories bests = new Factories(bestFactories);
+        Factories worst = new Factories(worstFactories);
+
+        Statistics statistics = new Statistics(bests.findBestFactory().getFactoryEfficiency(), worst.findWorstFactory().getFactoryEfficiency(), bests.avg(), bests.stdDeviation());
+        return statistics;
     }
 }
