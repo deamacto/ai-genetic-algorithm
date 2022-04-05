4c3422bede530f124779cabf8e7b75c2a08a35d3
Asia <pszon.joanna@gmail.com>
2022-04-05 16:14:00 +0200

    added roulette population and random population

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,20 +6,25 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        DataType dt = new DataType(Difficulty.FLAT);
+        DataType dt = new DataType(Difficulty.HARD);
 
         Statistics tournamentStats = TournamentPopulation(dt);
         System.out.println(tournamentStats);
 
+        Statistics rouletteStats = RoulettePopulation(dt);
+        System.out.println(rouletteStats);
+
+        Statistics randomStats = Random(dt);
+        System.out.println(randomStats);
     }
 
     public static Statistics TournamentPopulation(DataType dataType) {
         String populationName  = "tournament";
-        Population population = new Population(dataType, 100, populationName);
+        Population population = new Population(dataType, 500, populationName);
         ArrayList<FactoryEfficiency> bestFactories = new ArrayList<>();
         ArrayList<FactoryEfficiency> worstFactories = new ArrayList<>();
 
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < 50; i++) {
             bestFactories.add(population.getFactories().findBestFactory());
             worstFactories.add(population.getFactories().findWorstFactory());
 
@@ -31,8 +36,61 @@ public class Main {
 
             population = new Population(newPopulation, populationName);
 
-            population = population.crossover(0.6, populationName);
-            population.mutation(0.5, 1);
+            population = population.crossover(0.2, populationName);
+            population.mutation(0.1, 1);
+        }
+
+        Factories bests = new Factories(bestFactories);
+        Factories worst = new Factories(worstFactories);
+
+        return new Statistics(bests.findBestFactory().getFactoryEfficiency(), worst.findWorstFactory().getFactoryEfficiency(), bests.avg(), bests.stdDeviation());
+    }
+
+    public static Statistics RoulettePopulation(DataType dataType) {
+        String populationName  = "roulette";
+        Population population = new Population(dataType, 500, populationName);
+        ArrayList<FactoryEfficiency> bestFactories = new ArrayList<>();
+        ArrayList<FactoryEfficiency> worstFactories = new ArrayList<>();
+
+        for(int i = 0; i < 50; i++) {
+            bestFactories.add(population.getFactories().findBestFactory());
+            worstFactories.add(population.getFactories().findWorstFactory());
+
+            ArrayList<FactoryEfficiency> newPopulation = new ArrayList<>();
+
+            for(int j = 0; j < population.getFactories().getFactories().size(); j++) {
+                newPopulation.add(population.rouletteSelection().clone());
+            }
+
+            population = new Population(newPopulation, populationName);
+
+            population = population.crossover(0.2, populationName);
+            population.mutation(0.1, 1);
+        }
+
+        Factories bests = new Factories(bestFactories);
+        Factories worst = new Factories(worstFactories);
+
+        return new Statistics(bests.findBestFactory().getFactoryEfficiency(), worst.findWorstFactory().getFactoryEfficiency(), bests.avg(), bests.stdDeviation());
+    }
+
+    public static Statistics Random(DataType dataType) {
+        String populationName  = "random";
+        Population population = new Population(dataType, 500, populationName);
+        ArrayList<FactoryEfficiency> bestFactories = new ArrayList<>();
+        ArrayList<FactoryEfficiency> worstFactories = new ArrayList<>();
+
+        for(int i = 0; i < 25; i++) {
+            bestFactories.add(population.getFactories().findBestFactory());
+            worstFactories.add(population.getFactories().findWorstFactory());
+
+            ArrayList<FactoryEfficiency> newPopulation = new ArrayList<>();
+
+            for(int j = 0; j < population.getFactories().getFactories().size(); j++) {
+                newPopulation.add(population.getFactories().getFactories().get((int)(Math.random() * population.getFactories().getFactories().size())).clone());
+            }
+
+            population = new Population(newPopulation, populationName);
         }
 
         Factories bests = new Factories(bestFactories);
